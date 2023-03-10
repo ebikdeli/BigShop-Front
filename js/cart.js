7e70dd1df0971f98d5ef754126e93c8863c07760
ebikdeli <ebikdeli@gmail.com>
2023-03-10 18:43:48 +0330

    cart script completed

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,85 @@
+// Base cart template and script:
+// https://codepen.io/justinklemm/pen/kyMjjv
 import {addOne, minuseOne} from './functions.js';
 import {sendPostData} from './ajax.js';
 
 
+// * Funtions to 'remove', 'update Quantity' and 'reCalculate' items for the cart
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00; 
+var fadeTime = 300;
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+  let totalQuantity = 0;
+  
+  /* Sum up row total prices */
+  $('.cart--total--price--value').each(function () {
+    subtotal += parseFloat($(this).text());
+  });
+  /* Sum up total quantity */
+  Array.from(document.querySelectorAll('.cart--product--quantity')).forEach(inputElem => {
+    totalQuantity += Number(inputElem.value);
+  })
+  
+  /* Calculate totals */
+  var tax = subtotal * taxRate;
+  var shipping = (subtotal > 0 ? shippingRate : 0);
+  var total = subtotal + tax + shipping;
+  
+  /* Update totals display */
+  $('.totals-value').fadeOut(fadeTime, function() {
+    $('#cart-subtotal').html(subtotal.toFixed(0));
+    $('#cart-tax').html(tax.toFixed(0));
+    $('#cart-shipping').html(shipping.toFixed(0));
+    $('#cart-total').html(total.toFixed(0));
+    $('#cart-items').html(totalQuantity.toFixed(0));
+    if(total == 0){
+      $('.checkout').fadeOut(fadeTime);
+    }else{
+      $('.checkout').fadeIn(fadeTime);
+    }
+    $('.totals-value').fadeIn(fadeTime);
+  });
+}
+
+
+/* Update quantity */
+function updateQuantity(quantityInputElem)
+{
+  /* Calculate line price */
+  var productRow = $(quantityInputElem).parent().parent().parent();
+  var price = parseFloat(productRow.children('.cart--unit--price--column').children('.cart--unit--price--box').children('.cart--unit-price--value').text());
+  var quantity = $(quantityInputElem).val();
+  var linePrice = price * quantity;
+  /* Update line price display and recalc cart totals */
+  productRow.children('.cart--total--price--column').children('.cart--total--price--box').children('.cart--total--price--value').each(function () {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(0));
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });  
+}
+
+/* Remove item from cart */
+function removeItem(removeIcon)
+{
+  /* Remove row from DOM and recalc cart total */
+  var productRow = $(removeIcon).parent().parent().parent();
+  productRow.slideUp(fadeTime, function() {
+    productRow.remove();
+    recalculateCart();
+  });
+}
+
+
+// * Cart funtionality
 // Change product quantity
-const changeProductQuantity = document.querySelector('.cart--change--quantity');
+const changeProductQuantity = document.querySelector('#cart--product--list');
 
 changeProductQuantity.addEventListener('click', e => {
   // * Add quantity
@@ -19,6 +95,7 @@ changeProductQuantity.addEventListener('click', e => {
           if(data.status == '200'){
             console.log(data);
             addOne(elem, 15);
+            updateQuantity(elem);
           }
         })
         .catch(err => {
@@ -41,6 +118,7 @@ changeProductQuantity.addEventListener('click', e => {
           if(data.status == '200'){
             console.log(data);
             minuseOne(elem);
+            updateQuantity(elem);
           }
         })
         .catch(err => {
@@ -52,81 +130,48 @@ changeProductQuantity.addEventListener('click', e => {
 })
 
 
-// * Delete Item
-
-
-/* Set rates + misc */
-var taxRate = 0.05;
-var shippingRate = 15.00; 
-var fadeTime = 300;
-
-
-/* Assign actions */
-$('.cart--product-quantity input').change( function() {
-  updateQuantity(this);
-});
-
-$('.cart--product-removal button').click( function() {
-  removeItem(this);
-});
-
-
-/* Recalculate cart */
-function recalculateCart()
-{
-  var subtotal = 0;
-  
-  /* Sum up row totals */
-  $('.cart--product').each(function () {
-    subtotal += parseFloat($(this).children('.cart--product-line-price').text());
-  });
-  
-  /* Calculate totals */
-  var tax = subtotal * taxRate;
-  var shipping = (subtotal > 0 ? shippingRate : 0);
-  var total = subtotal + tax + shipping;
-  
-  /* Update totals display */
-  $('.totals-value').fadeOut(fadeTime, function() {
-    $('#cart-subtotal').html(subtotal.toFixed(2));
-    $('#cart-tax').html(tax.toFixed(2));
-    $('#cart-shipping').html(shipping.toFixed(2));
-    $('#cart-total').html(total.toFixed(2));
-    if(total == 0){
-      $('.checkout').fadeOut(fadeTime);
-    }else{
-      $('.checkout').fadeIn(fadeTime);
+// Integration of pure JS and Jquery!!!
+// $('.cart--product-removal').click( function() {
+$('.cart--remove--icon').click( function() {
+  Swal.fire({
+    title: 'حذف محصول',
+    text: "آیا از حذف این محصول از سبد خرید خود مطمئنید؟",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#37eb34',
+    cancelButtonColor: '#eb3434',
+    confirmButtonText: 'بله',
+    cancelButtonText: 'خیر',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let data = {product_id: this.getAttribute('data-product-id')};
+      sendPostData('http://127.0.0.1:8000/delete-product-cart', data, 'مشکلی پیش آمده')
+      .then(data => {
+        console.log(data)
+        if(data.status == 200){
+          removeItem(this);
+          Swal.fire(
+            'پاک شد!',
+            'محصول از سبد خرید حذف شد',
+            'success'
+          )
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'مشکلی پیش آمده',
+          text: 'محصول حذف نشد',
+        })
+      })
     }
-    $('.totals-value').fadeIn(fadeTime);
-  });
-}
-
-
-/* Update quantity */
-function updateQuantity(quantityInput)
-{
-  /* Calculate line price */
-  var productRow = $(quantityInput).parent().parent();
-  var price = parseFloat(productRow.children('.cart--product-price').text());
-  var quantity = $(quantityInput).val();
-  var linePrice = price * quantity;
-  /* Update line price display and recalc cart totals */
-  productRow.children('.cart--product-line-price').each(function () {
-    $(this).fadeOut(fadeTime, function() {
-      $(this).text(linePrice.toFixed(2));
-      recalculateCart();
-      $(this).fadeIn(fadeTime);
-    });
-  });  
-}
-
-/* Remove item from cart */
-function removeItem(removeButton)
-{
-  /* Remove row from DOM and recalc cart total */
-  var productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
-    productRow.remove();
-    recalculateCart();
-  });
-}
+    else{
+      Swal.fire('محصول در سبد خرید باقی ماند')
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    alert('SWAL کار نمی کند');
+  })
+});
